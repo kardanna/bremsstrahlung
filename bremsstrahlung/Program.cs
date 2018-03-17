c10b7044482426ca460fd1592b89d88d9d27f3cd
Юрий Миргород <karas-mailbox@tut.by>
2018-03-18 00:12:32 +0300

    Исправлена лицензионная информация
    Исправлена лицензионная информация

--- bremsstrahlung/Program.cs
+++ bremsstrahlung/Program.cs
@@ -1,4 +1,4 @@
-﻿//     Copyright 2018 (c), Mirgorod Yuri
+﻿//     Copyright 2018 (C), Mirgorod Yuri
 //     
 //     This file is part of Bremsstahlung.
 // 
@@ -7,13 +7,13 @@
 //     the Free Software Foundation, either version 3 of the License, or
 //     (at your option) any later version.
 // 
-//     Foobar is distributed in the hope that it will be useful,
+//     Bremsstahlung is distributed in the hope that it will be useful,
 //     but WITHOUT ANY WARRANTY; without even the implied warranty of
 //     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 //     GNU General Public License for more details.
 // 
 //     You should have received a copy of the GNU General Public License
-//     along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+//     along with Bremsstahlung.  If not, see <http://www.gnu.org/licenses/>.
 using System;
 using System.Collections.Generic;
 using System.Linq;
